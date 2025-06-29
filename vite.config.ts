0b19eccb9cee0eb634dbbbc0a42d6c3a5e4b4d49
FrancescoCap <francesco.caputo0209@gmail.com>
2025-06-29 11:19:17 +0200

    set gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mobiledevsos.github.io/',
+  build: {
+    outDir: 'dist', // Questa è la cartella di output di default di Vite
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
